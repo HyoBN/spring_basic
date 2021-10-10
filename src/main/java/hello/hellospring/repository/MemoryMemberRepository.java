584b69fa3827ce8c636b41b03c5a8608f3438a48
양효빈 <qls0786@naver.com>
2021-10-10 08:02:23 +0000

    test 에러 해결

--- src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
+++ src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
@@ -1,33 +1,21 @@
 package hello.hellospring.repository;
-
+import hello.hellospring.domain.Member;
 import java.util.*;
 
-import hello.hellospring.domain.Member;
-
-public class MemoryMemberRepository implements MemberRepository{
-    
+public class MemoryMemberRepository implements MemberRepository {
     private static Map<Long, Member> store = new HashMap<>();
     private static long sequence = 0L;
-    
-    
+
     @Override
-    public Member save(Member member){
-        member.setId(+sequence);
-        store.put(member.getId(), member);        
-        return member;
+    public Member save(Member member) {
+        member.setId(++sequence);
+        store.put(member.getId(), member);
+    return member;
     }
-    
+
     @Override
-    public Optional<Member> findById(Long id){
+    public Optional<Member> findById(Long id) {
         return Optional.ofNullable(store.get(id));
-    }
-    
-    @Override
-    public Optional<Member> findByName(String name){
-        store.values().stream()
-            .filter(member -> member.getName().equals(name))
-            .findAny();
-        return Optional.empty();
     }
     
     @Override
@@ -35,7 +23,14 @@ public class MemoryMemberRepository implements MemberRepository{
         return new ArrayList<>(store.values());
     }
     
-    public void clearStore(){
+    @Override
+    public Optional<Member> findByName(String name) {
+        return store.values().stream()
+        .filter(member -> member.getName().equals(name))
+        .findAny();
+    }
+    
+    public void clearStore() {
         store.clear();
     }
 }
