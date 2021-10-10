93fd9da86f1dc0a30f4150b7390e8eaced683b8f
양효빈 <qls0786@naver.com>
2021-10-10 10:45:49 +0000

    회원 서비스 추가, 테스트

---
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -0,0 +1,41 @@
+package hello.hellospring.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.repository.MemoryMemberRepository;
+
+public class MemberService{
+    private final MemberRepository memberRepository;
+    
+    // Dependency Injection(DI, 의존성 주입)
+    public MemberService(MemberRepository memberRepository){
+        this.memberRepository = memberRepository;
+    }
+    
+
+    //회원 가입.
+    public Long join(Member member){
+        //회원 이름 중복 방지.
+        memberRepository.findByName(member.getName())
+            .ifPresent(m -> {
+                throw new IllegalStateException("이미 존재하는 회원입니다.");
+            });
+        
+        memberRepository.save(member);
+        return member.getId();
+    }
+    
+    //전체 회원 조회.
+    public List<Member> findMembers() {
+         return memberRepository.findAll();
+    }
+    
+    
+    public Optional<Member> findOne(Long memberId){
+        return memberRepository.findById(memberId);
+        
+    }
+}
